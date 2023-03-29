2e4ea7aee120c38c11ac738737900f5a94cb6d9c
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-03-29 23:02:33 +0300

    homework 3/5

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,10 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   groupName = 'Healthy lifestyle';
-  newGroupName: string = '';
 
-  editGroupName() {
-    this.groupName = this.newGroupName;
-    this.newGroupName = '';
+  editGroupName(newName: string) {
+    this.groupName = newName;
   }
 }
