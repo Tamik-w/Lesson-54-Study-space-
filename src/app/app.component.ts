e1a3928ccee7fca27a1ac028b42348553fa66a45
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-03-29 23:15:10 +0300

    homework 4/5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor( public users: UsersService){}
+  constructor(public users: UsersService) {}
 
-
+  updateUser(info: {name: string, status: string}, user: {name: string, status: string, url: string, id: number}) {
+    user.name = info.name;
+    user.status = info.status;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +6,12 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-  @Input() name = ''
-  @Input() status = ''
-  @Input() img = ''
+  @Input() name = '';
+  @Input() status = '';
+  @Input() img = '';
+  @Output() update = new EventEmitter<{name: string, status: string}>();
+
+  updateInfo(name: string, status: string) {
+    this.update.emit({name, status});
+  }
 }
