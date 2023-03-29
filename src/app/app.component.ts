c124a30e3fe945333bf32a086c6efef3b3b64297
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-03-29 22:57:32 +0300

    homework 2/5

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { UsersService } from './service/users.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor( public users: UsersService){}
+
+
+}
